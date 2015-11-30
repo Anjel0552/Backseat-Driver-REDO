c20142e844c615ed930950702ee863587454405a
Anjel0552 <anjelvillafranco0552@gmail.com>
2015-11-30 13:02:11 -0500

    init app
    New app from scratch

---
+++ Backseat Driver/MapViewController.swift
@@ -0,0 +1,58 @@
+//
+//  ViewController.swift
+//  Backseat Driver
+//
+//  Created by Anjel Villafranco on 11/30/15.
+//  Copyright © 2015 Anjel Villafranco. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class MapViewController: UIViewController, CLLocationManagerDelegate {
+    
+    
+    @IBOutlet weak var myMapView: MKMapView!
+    
+    let lManager = CLLocationManager()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // need to ask for location
+        lManager.requestWhenInUseAuthorization()
+        
+        lManager.delegate = self
+        
+        // Show pretty blue dot
+        myMapView.showsUserLocation = true
+        
+        //        lManager.startUpdatingLocation()
+        
+        lManager.requestLocation()
+        
+    }
+    
+    func locationManager(manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        
+        for location in locations {
+            
+            print(location.coordinate.latitude, location.coordinate.longitude)
+            
+        }
+    }
+    
+    func locationManager(manager: CLLocationManager, didFailWithError error: NSError) {
+        
+        print(error)
+    }
+    
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
+
